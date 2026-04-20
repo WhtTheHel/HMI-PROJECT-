1bb15ef4e31784960beaa27098cd2aa22d436326
WhtTheHel <karmanuserbot@gmail.com>
2026-04-20 21:33:25 +0700

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+// public/firebase.js
+const firebaseConfig = {
+    apiKey: "ISI_API_KEY_ANDA_DISINI",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
+};
+
+// Initialize Firebase (Imported via CDN in HTML)
+// Note: Ini akan dipanggil di app.js menggunakan Firebase SDK modular atau compat
